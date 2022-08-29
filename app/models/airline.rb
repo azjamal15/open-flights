8d4dd044950cd87988535f99ee07281ddd984784
azjamal15 <azamj13@gmail.com>
2022-08-29 15:59:33 -0500

    Creating seed data or app

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -4,10 +4,10 @@ class Airline < ApplicationRecord
   before_create :slugify #sets slug for airline before we create to database 
 
   def slugify 
-    self.slug = name.parametarize #takes airline name, creates a url safe version of the name and sets to slug value 
+    self.slug = name.parameterize #takes airline name, creates a url safe version of the name and sets to slug value 
   end 
 
   def avg_score 
-    reviews.average(:score).round(2).to_f #average of score, roudn to two places, convert to float
+    reviews.average(:score).round(2).to_f #average of score, round to two places, convert to float
   end 
 end
